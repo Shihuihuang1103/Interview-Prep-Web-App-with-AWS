2ed95f3b12b008e9ed9669ffdbd1d01f9d09b200
Shihui Huang <sh4480@nyu.edu>
2023-11-23 03:14:54 -0500

    home page

---
+++ interview-xpert/app/components/navHome.js
@@ -0,0 +1,32 @@
+import Link from 'next/link';
+
+const NavHome = () => {
+    return (
+      <nav className="bg-blue-800">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="flex justify-between">
+            <div className="flex space-x-4">
+              {/* Logo and brand name */}
+              <div>
+                <a href="/" className="flex items-center py-5 px-2 text-white">
+                  <span className="font-semibold text-xl">InterviewXpert</span>
+                </a>
+              </div>
+              {/* Primary Nav */}
+              <div className="hidden md:flex items-center space-x-1">
+                <a href="#" className="py-5 px-3 text-white">About</a>
+                <a href="#" className="py-5 px-3 text-white">Contact</a>
+              </div>
+            </div>
+            {/* Secondary Nav */}
+            <div className="hidden md:flex items-center space-x-1">
+              <a href="signup" className="py-2 px-3 bg-blue-700 text-white rounded">Sign Up</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
+  };
+  
+  export default NavHome;
+  
---
+++ interview-xpert/app/signup/page.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavHome from '../components/navHome';
+const Signup = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <NavHome />
+        <div>
+          {/* Your signup form and related JSX goes here */}
+          <h1>Sign Up</h1>
+        </div>
+    </div>
+  );
+};
+
+export default Signup;
