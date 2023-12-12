862a21033f4d62a63643ccef960aacd627ed9bb0
Shihui Huang <sh4480@nyu.edu>
2023-12-12 05:37:26 -0500

    schedule interview api call

--- interview-xpert/app/interviewee/page.js
+++ interview-xpert/app/interviewee/page.js
@@ -14,11 +14,39 @@ const BookInterview = () => {
     const [focus, setFocus] = useState('');
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
+    const [duration, setDuration] = useState('');
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        // Implement your register logic here
-        console.log(username, password);
+        
+        const interviewSession = {
+           focus,
+           date,
+           time,
+           duration
+        };
+
+        const apiGatewayUrl = 'https://6lpyoj0hu8.execute-api.us-east-1.amazonaws.com/test/scheduleInterviews/${username}';
+
+        try{
+        const response = await fetch(apiGatewayUrl, {
+            method:'POST',
+            mode:'cors',
+            headers:{
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(interviewSession),
+        });
+
+        if(!response.ok){
+            throw new Error('Network response was not ok.');
+        }
+
+        const data = await response.json();
+        console.log("Interview successfully scheduled", data);
+        } catch(error){
+        console.error('Interview schedule failed', error);
+        }
       };
     
       return (
@@ -32,14 +60,14 @@ const BookInterview = () => {
                             Schedule A New Interview 
                         </h1>
                         <br/>
-                        <label htmlFor="focus" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+                        <label htmlFor="focus" className="block text-sm font-medium text-gray-700">Choose your interview type:</label>
                         <select 
                           id="focus" 
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           value = {focus}
                           onChange={(e)=>setFocus(e.target.value)}
                         >
-                        <option value=''>Choose your interview focus</option>
+    
                         <option value="Data Structure and Algorithm">Data Structure and Algorithm</option>
                         <option value="System Design">System Design</option>
                         <option value="Behavioral Questions">Behavioral Questions</option>
@@ -69,6 +97,23 @@ const BookInterview = () => {
                             value={time}
                             onChange={(e) => setTime(e.target.value)}
                         />
+                        </div>
+                        
+                       
+                        <label htmlFor="duration" className="block text-sm font-medium text-gray-700">Interview Sesion: </label>
+                        <select 
+                          id="duration" 
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          value = {duration}
+                          onChange={(e)=>setDuration(e.target.value)}
+                        >
+                        <option value="30">30 minutes</option>
+                        <option value="60">60 minutes</option>
+                        <option value="90">90 minutes</option>
+                        </select>
+                        
+                        <div>
+                             <br/>
                         </div>
                         <div>
                         <button type="submit" className="w-full text-white bg-blue-800 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 font-medium rounded-md text-sm px-5 py-2.5 text-center">
