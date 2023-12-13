380b2cb4eff032b74267b67d900a2ca217930291
Shihui Huang <sh4480@nyu.edu>
2023-12-13 00:20:36 -0500

    fetch upcoming interviews

--- interview-xpert/app/interviewee/scheduled/page.js
+++ interview-xpert/app/interviewee/scheduled/page.js
@@ -1,9 +1,37 @@
 "use client";
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavInterviewee from '@/app/components/navInterviewee';
+import UserPool from '@/app/services/UserPool';
 
 const ScheduledInterviews = () => {
+    const[interviews, setInterviews] = useState([]);
+
+    useEffect(()=> {
+        const fetchInterviews = async() =>{
+            try{
+                const user = UserPool.getCurrentUser();
+
+                if(user){
+                    const username = user.getUsername();
+                    console.log('current user:', username);
+                    const apiGatewayUrl = `https://6lpyoj0hu8.execute-api.us-east-1.amazonaws.com/test/getinterviews/${username}`;
+
+                    const response = await fetch(apiGatewayUrl);
+                    if(!response.ok){
+                        throw new Error('Failed to fetch interviews');
+                    }
+                    const data = await response.json();
+                    const upcomingInterviews = data.filter(
+                        interview => new Date(`${interview.date} ${interview.time}`) > new Date());
+                    setInterviews(upcomingInterviews);
+                }
+            } catch (error){
+                console.error('Error fetching users:', error);
+            }
+        };
+        fetchInterviews();
+    },[]);
 
     
       return (
@@ -14,6 +42,21 @@ const ScheduledInterviews = () => {
                     <h1 className='text-center font-bold text-2xl text-blue-800'> 
                         Your upcoming interviews:
                     </h1>
+                    {interviews.length > 0 ? (
+                        <ul>
+                        {interviews.map(interview => (
+                            <li key={interview.id}>
+                            <p>Interviewer: {interview.interviewerName}</p>
+                            <p>Focus: {interview.focus}</p>
+                            <p>Date: {interview.date}</p>
+                            <p>Time: {interview.time}</p>
+                            <p>Duration: {interview.duration} </p>
+                            </li>
+                        ))}
+                        </ul>
+                        ) : (
+                            <p>No upcoming interviews</p>
+                         )}
                 </div>
             </div>
         </div>
