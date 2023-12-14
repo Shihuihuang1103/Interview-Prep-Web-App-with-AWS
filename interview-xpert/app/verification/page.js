52f508f0cf3de0014b800cfaea83dea7a6b86a5d
lyuan45 <lyuan45@wisc.edu>
2023-12-14 01:12:13 -0500

    Update page.js

--- interview-xpert/app/verification/page.js
+++ interview-xpert/app/verification/page.js
@@ -1,4 +1,5 @@
 "use client";
+import React from 'react';
 import { useState } from 'react';
 import NavHome from '@/app/components/navHome';
 import { confirmSignUp } from '../services/confirmSignUp'; 
