4e172b66c52fbcace8b9d236c3216bbf14ffaeb0
Shihui Huang <sh4480@nyu.edu>
2023-12-14 01:59:47 -0500

    interviewee profile fix

--- interview-xpert/app/interviewee/profile/page.js
+++ interview-xpert/app/interviewee/profile/page.js
@@ -6,8 +6,7 @@ import UserPool from '@/app/services/UserPool';
 
 
 const IntervieweeProfile = () => {
-
-     const[userAttributes, setUserAttributes] = useState(null);
+     const[userAttributes, setUserAttributes] = useState('');
 
      useEffect(()=>{
         const fetchUserProfile = async() => {
@@ -26,12 +25,14 @@ const IntervieweeProfile = () => {
                     throw new Error('Failed to fetch user profile');
                 }
                 const data = await response.json();
-                console.log(data);
-                setUserAttributes(data);
+                console.log("Received Data:", data.body);
+                setUserAttributes(JSON.parse(data.body));
+
             }catch(error){
                 console.error('Error fetching user profile:', error);
             }
          }
+         
         };
         fetchUserProfile();
     },[]);
@@ -40,20 +41,21 @@ const IntervieweeProfile = () => {
         <div className="flex flex-col min-h-screen">
             <NavInterviewee />
             <div className="flex min-h-screen bg-gray-50 justify-left items-center">
-                <div className="p-6 max-w-sm w-full bg-blue-50 shadow-md rounded-md">
+                <div className="p-6 max-w-xl w-full bg-blue-50 shadow-md rounded-md">
                     <h1 className='text-center font-bold text-2xl text-blue-800'> 
                         My Profile:
                     </h1>
-                    {userAttributes && (
-                        <div>
-                            <p>First name: {userAttributes.firstname}</p>
-                            <p>Last name: {userAttributes.lastname}</p>
-                            <p>Username: {userAttributes.username}</p>
-                            <p>Email: {userAttributes.email}</p>
-                            <p>School: {userAttributes.school}</p>
-                            <p>Field: {userAttributes.field}</p>
-                            <p>Year of Experience: {userAttributes.yoe}</p>
-                            <p>About: {userAttributes.bio}</p>
+                    <br/>
+                    {userAttributes &&  (
+                        <div >
+                            <p className="text-md mb-2">First name: {userAttributes.intervieweeProfile.firstname}</p>
+                            <p className="text-md mb-2">Last name: {userAttributes.intervieweeProfile.lastname}</p>
+                            <p className="text-md mb-2">Username: {userAttributes.intervieweeProfile.username}</p>
+                            <p className="text-md mb-2">Email: {userAttributes.intervieweeProfile.email}</p>
+                            <p className="text-md mb-2">School: {userAttributes.intervieweeProfile.school}</p>
+                            <p className="text-md mb-2">Field: {userAttributes.intervieweeProfile.field}</p>
+                            <p className="text-md mb-2">Year of Experience: {userAttributes.intervieweeProfile.yoe}</p>
+                            <p className="text-md mb-2">About: {userAttributes.intervieweeProfile.bio}</p>
                         </div>
                     )}
                 </div>
