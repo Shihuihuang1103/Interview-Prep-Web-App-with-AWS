9b2f43865b44c3de3c972760839e0e409bc0759a
Shihui Huang <sh4480@nyu.edu>
2023-12-14 04:50:05 -0500

    getInterviews & feedback

--- interview-xpert/app/interviewee/finished/page.js
+++ interview-xpert/app/interviewee/finished/page.js
@@ -7,10 +7,23 @@ import UserPool from '@/app/services/UserPool';
 
 const FinishedInterviews = () => {
     const[interviews, setInterviews] = useState([]);
+    const [showFeedbackForm, setShowFeedbackForm] = useState(false);
+    const [selectedInterviewId, setSelectedInterviewId] = useState(null);
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
 
     const handleFeedback = (interviewId) => {
-        console.log(`Feedback for interview ${interviewId}`);
-    }
+        setShowFeedbackForm(true);
+        setSelectedInterviewId(interviewId);
+      };
+    
+    const handleSubmitFeedback = () => {
+        //implement logit to submit feedback
+        setShowFeedbackForm(false);
+        setSelectedInterviewId(null);
+        setRating(0);
+        setComment('');
+    };
 
     useEffect(()=> {
         const fetchInterviews = async() =>{
@@ -28,9 +41,23 @@ const FinishedInterviews = () => {
                         throw new Error('Failed to fetch interviews');
                     }
                     const data = await response.json();
-                    const finishedInterviews = data.filter(
-                        interview => new Date(`${interview.date} ${interview.time}`) < new Date());
+                    const dataBody = JSON.parse(data.body);
+
+                    const finishedInterviews = dataBody.filter(
+                        interview => {
+                            const interviewDate = new Date(interview.date);
+                            const interviewTimeInSeconds = interview.time;
+                            const interviewDateTime = new Date(interviewDate);
+                          
+                            // Set the time in milliseconds since midnight
+                            interviewDateTime.setSeconds(interviewTimeInSeconds);
+                          
+                            // Compare with the current time
+                            console.log(new Date());
+                            return interviewDateTime < new Date();                       
+                        });
                     setInterviews(finishedInterviews);
+                    
                 }
             } catch (error){
                 console.error('Error fetching users:', error);
@@ -44,21 +71,25 @@ const FinishedInterviews = () => {
         <div className="flex flex-col min-h-screen">
             <NavInterviewee />
             <div className="flex min-h-screen bg-gray-50 justify-left items-center">
-                <div className="p-6 max-w-sm w-full bg-blue-50 shadow-md rounded-md">
+                <div className="p-6 max-w-xl w-full bg-blue-50 shadow-md rounded-md">
                     <h1 className='text-center font-bold text-2xl text-blue-800'> 
                         Your Completed Interviews:
                     </h1>
+                    <br/>
                     {interviews.length > 0 ? (
                         <ul>
                         {interviews.map(interview => (
-                            <li key={interview.id}>
+                            <li key={interview.id} className="mb-6 border-b pb-4">
                             <p>Interviewer: {interview.interviewerName}</p>
                             <p>Focus: {interview.focus}</p>
                             <p>Date: {interview.date}</p>
                             <p>Time: {interview.time}</p>
                             <p>Duration: {interview.duration} </p>
-                            <button onClick={() => handleFeedback(interview.id)}>
-                             Give Feedback
+                            <br/>
+                            <button 
+                            onClick={() => handleFeedback(interview.id)} 
+                            className="w-full text-white bg-blue-800 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 font-medium rounded-md text-sm px-5 py-2.5 text-center">
+                            Give Feedback
                             </button>
                             </li>
                         ))}
@@ -68,6 +99,52 @@ const FinishedInterviews = () => {
                          )}
                 </div>
             </div>
+
+            {/* Feedback form */}
+      {showFeedbackForm && (
+        <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center">
+          <div className="bg-white p-8 rounded-md shadow-md max-w-md">
+            <h2 className="text-xl font-semibold mb-4">Feedback for Interview</h2>
+            <div className="mb-4">
+              <label htmlFor="rating" className="block text-sm font-medium text-gray-700">Rating:</label>
+              <input
+                type="number"
+                id="rating"
+                name="rating"
+                min="1"
+                max="5"
+                value={rating}
+                onChange={(e) => setRating(e.target.value)}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="comment" className="block text-sm font-medium text-gray-700">Comment:</label>
+              <textarea
+                id="comment"
+                name="comment"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                className="mt-1 p-2 border rounded-md w-full"
+              />
+            </div>
+            <div className="flex justify-end">
+              <button
+                onClick={handleSubmitFeedback}
+                className="bg-blue-800 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+              >
+                Submit Feedback
+              </button>
+              <button
+                onClick={() => setShowFeedbackForm(false)}
+                className="ml-2 text-gray-600 hover:text-gray-800"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
         </div>
       );
 };
--- interview-xpert/app/interviewee/scheduled/page.js
+++ interview-xpert/app/interviewee/scheduled/page.js
@@ -17,18 +17,42 @@ const ScheduledInterviews = () => {
                     const userID = username
                     console.log('current user:', username);
                     const apiGatewayUrl = `https://6lpyoj0hu8.execute-api.us-east-1.amazonaws.com/test/getInterviews/${userID}`;
-
+                    
                     const response = await fetch(apiGatewayUrl);
                     if(!response.ok){
                         throw new Error('Failed to fetch interviews');
                     }
                     const data = await response.json();
-                    const upcomingInterviews = data.filter(
-                        interview => new Date(`${interview.date} ${interview.time}`) > new Date());
+                    console.log(data.body);
+                    const dataBody = JSON.parse(data.body);
+
+                    if(Array.isArray(dataBody)){
+                        const upcomingInterviews = dataBody.filter(
+                        interview => {
+                            const interviewDate = new Date(interview.date);
+                            const interviewTimeInSeconds = interview.time;
+                            const interviewDateTime = new Date(interviewDate);
+                          
+                            // Set the time in milliseconds since midnight
+                            interviewDateTime.setSeconds(interviewTimeInSeconds);
+                          
+                            // Compare with the current time
+                            console.log(new Date());
+                            return interviewDateTime > new Date();
+                           
+
+                    });
                     setInterviews(upcomingInterviews);
+                    } else{
+                        console.error('Invalid data format: not an array');
+                        setInterviews([]);
+                    }
+                    //setInterviews(dataBody);
+                    console.log(interviews);
                 }
             } catch (error){
                 console.error('Error fetching users:', error);
+                setInterviews([]);
             }
         };
         fetchInterviews();
@@ -39,14 +63,17 @@ const ScheduledInterviews = () => {
         <div className="flex flex-col min-h-screen">
             <NavInterviewee />
             <div className="flex min-h-screen bg-gray-50 justify-left items-center">
-                <div className="p-6 max-w-sm w-full bg-blue-50 shadow-md rounded-md">
+                <div className="p-6 max-w-xl w-full bg-blue-50 shadow-md rounded-md">
                     <h1 className='text-center font-bold text-2xl text-blue-800'> 
                         Your upcoming interviews:
                     </h1>
+                    <br/>
                     {interviews.length > 0 ? (
+                        <div>
                         <ul>
+                        <br/>
                         {interviews.map(interview => (
-                            <li key={interview.id}>
+                            <li key={interview.id} className="mb-6 border-b pb-4">
                             <p>Interviewer: {interview.interviewerName}</p>
                             <p>Focus: {interview.focus}</p>
                             <p>Date: {interview.date}</p>
@@ -56,6 +83,14 @@ const ScheduledInterviews = () => {
                             </li>
                         ))}
                         </ul>
+                        <a  
+                         href="https://app.chime.aws/"
+                         target="_blank" 
+                         className="w-full text-white bg-blue-800 hover:bg-blue-700 focus:ring-4 focus:ring-blue-800 font-medium rounded-md text-sm px-5 py-2.5 text-center">
+                            Enter Your Interview Session
+                         </a>
+                        </div>
+                        
                         ) : (
                             <p>No upcoming interviews</p>
                          )}
